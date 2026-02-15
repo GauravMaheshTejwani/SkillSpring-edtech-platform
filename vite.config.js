0a6938b819e4658f4d484e08213076b75d46b1d9
GauravMaheshTejwani <gauravtejwani7559@gmail.com>
2026-02-15 17:02:40 +0530

    updated target

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        //target: 'http://localhost:4000',
+        target: 'https://skillspring-edtech-platform-1.onrender.com/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
